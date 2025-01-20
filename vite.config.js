7a8d3f15082e29e6ee68a34ccc67566045418436
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-01-21 01:03:43 +0800

    Updated main entry point foodexer/src

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: 'src', // Vite will use the 'src' folder as the root
+  root: 'foodexer/src', // Vite will use the 'foodexer/src' folder as the root
   build: {
     outDir: '../dist', // Output folder for the build (relative to the root)
 
     // Rollup specific configurations
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'), // Your main entry point
+        main: resolve(__dirname, 'foodexer/src/index.html'), // Your main entry point
       },
     },
   },
